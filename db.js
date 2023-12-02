e4bb3a920db3b732bd0857ef25cb046a7182c7f0
Gaurav <gauravsaraiwala20@gmail.com>
2023-12-03 03:22:42 +0530

    Second Commit:

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const pool = new Pool({
-    connectionString: "postgres://task_uggm_user:7VyddeNiG5cNJoJV5iKICjqNgnxLe8pc@dpg-cllndmcjtl8s73ep3ba0-a.oregon-postgres.render.com:5432/task_uggm",
+    connectionString: process.env.password,
     ssl: true
 })
 
