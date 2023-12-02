788e731a5cecef2240521aedef2f5ff1dc445302
Gaurav <gauravsaraiwala20@gmail.com>
2023-12-03 03:15:12 +0530

    Second Commit:

--- db.js
+++ db.js
@@ -3,11 +3,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const pool = new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"task",
-    password:process.env.password,
-    port: 5432
+    connectionString: "postgres://task_uggm_user:7VyddeNiG5cNJoJV5iKICjqNgnxLe8pc@dpg-cllndmcjtl8s73ep3ba0-a.oregon-postgres.render.com:5432/task_uggm",
+    ssl: true
 })
 
 module.exports = pool
